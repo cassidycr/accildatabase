d5e84376252794ff0d438d2aab569c0ac19ea699
Cassidy Reid <97694302+cassidycr@users.noreply.github.com>
2025-06-18 10:11:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+st.set_page_config(page_title="Library Instruction App", initial_sidebar_state="expanded")
 st.title("Library Instruction App")
 st.write("Welcome to the ACC Library Instruction Database App!")
 st.write("Use the sidebar to navigate to Dashboard, Instruction Form, or Sessions.")
