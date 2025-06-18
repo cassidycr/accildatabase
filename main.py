c873f21c3fa32547749f1af08780da600e540b8e
Cassidy Reid <97694302+cassidycr@users.noreply.github.com>
2025-06-18 09:58:18 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Library Instruction App")
+st.write("Welcome to the ACC Library Instruction Database App!")
+st.write("Use the sidebar to navigate to Dashboard, Instruction Form, or Sessions.")
